2930967368c80f52c23289555170bf9fb2a7c6f6
Emmanuel Fabella <efrohibire@gmail.com>
2025-01-17 00:17:43 +0800

    feat: beautify home page and interaction, added jost font and new styles

---
+++ client/src/assets/mic.jsx
@@ -0,0 +1,12 @@
+export default function mic() {
+  return (
+    <div>
+      <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="5.34882" y="0.232559" width="9.09561" height="2" rx="1" fill="#33363F"/>
+      <rect x="0.232544" y="6.23256" width="19.3282" height="2" rx="1" fill="#33363F"/>
+      <rect x="0.232544" y="12.2326" width="19.3282" height="2" rx="1" fill="#33363F"/>
+      <rect x="5.34882" y="18.2326" width="9.09561" height="2" rx="1" fill="#33363F"/>
+      </svg>
+    </div>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,16 +6,18 @@ import {
   Route, 
   RouterProvider 
 } from 'react-router-dom';
-
 import './index.css'
+
 import Dashboard from './components/desktop/dashboard';
 import Home from './routes/home';
+import View from './routes/view';
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route>
       <Route index element={<Home />} />
       <Route path="home" element={<Home />} />
+      <Route path="/view" element={<View />} />
       <Route path="/desktop" element={<Dashboard />} />
     </Route>
   )
