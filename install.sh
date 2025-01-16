b2a7976b70f91b1898e9b69663a87a8d26db4b94
Emmanuel Fabella <efrohibire@gmail.com>
2025-01-16 15:10:02 +0800

    fix: configured install.s

--- install.sh
+++ install.sh
@@ -3,24 +3,23 @@
 echo "Adding upstream to your fork..."
 git remote add upstream https://github.com/MasterTraits/sasamahan-ka-sa-qc.git
 
-# 
 #   ONLY UNCOMMENT THIS IF YOU ARE USING PYTHON
-#
-# echo "Creating virtual environment..."
-# cd backend
-# py -m venv .venv
 
-# echo "Activating virtual environment..."
-# if source .venv/Scripts/activate
-# then
-#     echo "Virtual environment activated."
-# else
-#     .venv/Scripts/activate
-# fi
+echo "Creating virtual environment..."
+cd server
+py -m venv .venv
 
-# echo "Installing dependencies..."
-# pip install -r requirements.txt
-# cd ..
+echo "Activating virtual environment..."
+if source .venv/Scripts/activate
+then
+    echo "Virtual environment activated."
+else
+    .venv/Scripts/activate
+fi
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+cd ..
 
 # Install frontend dependencies
 cd client
