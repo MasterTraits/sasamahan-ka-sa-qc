698337d4078a67d67b2dd961bd38a063ebed8d2f
Shunke4444 <jihadtejam867@gmail.com>
2025-01-17 09:37:28 +0800

    functioning ai chat

--- client/src/components/features/aiChat.jsx
+++ client/src/components/features/aiChat.jsx
@@ -1,23 +1,36 @@
-import React, { useContext } from "react";
-import { UserInputContext } from "@/contexts/UserInputContext"; // Import context
+import React, { useContext, useState, useEffect } from "react";
+import { UserInputContext } from "@/contexts/UserInputContext";
 import Header from "./aiChatComponents/header";
 import Footer from "./aiChatComponents/footer";
 import UserChatBubble from "./aiChatComponents/userChatBubble";
-
-
+import runChat from "@/config/gemini";
+import AiChatBubble from "./aiChatComponents/aiChatBubble";
 
 export default function AiChat() {
   const { userInput } = useContext(UserInputContext);
+  const { aiResponse, setAiResponse } = useContext(UserInputContext);
+
+  
+  const fetchAIResponse = async () => {
+    if (userInput) {
+      const response = await runChat(userInput);
+      setAiResponse(response);
+    }
+  };
+
+  useEffect(() => {
+    fetchAIResponse();
+  }, [userInput]);
 
   return (
     <main className="h-screen w-[25%] shadow-xl flex flex-col">
       <Header />
       <section className="p-4 flex-grow">
         <h1>TITLE TEXT HERE</h1>
-        {userInput &&(
-          <UserChatBubble/>
+        {userInput && <UserChatBubble />}
+        {aiResponse && (
+          <AiChatBubble />
         )}
-        
       </section>
       <Footer />
     </main>
--- client/src/components/features/aiChatComponents/aiChatBubble.jsx
+++ client/src/components/features/aiChatComponents/aiChatBubble.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { UserInputContext } from '@/contexts/UserInputContext';
+import React, {useContext}from 'react'
 
 export default function AiChatBubble() {
+  const {aiResponse } = useContext(UserInputContext);
   return (
     <main className="flex justify-start">
-    <div className="bg-gray-300 rounded-lg rounded-tr-none p-2 inline-block max-w-[60%] ">    
-        <p>{userInput}</p>
+    <div className="bg-blue-300 shadow-md rounded-lg rounded-t-none p-2 inline-block max-w-[60%] ">    
+        <p>{aiResponse}</p>
     </div>
 </main>
   )
