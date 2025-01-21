c1e0c4bfa535ba0eeff3c5ffc68671ec544a7dfb
Emmanuel Fabella <efrohibire@gmail.com>
2025-01-21 15:22:58 +0800

    feat: redirect page

---
+++ client/src/routes/redirect.jsx
@@ -0,0 +1,15 @@
+
+export default function redirect() {
+  setTimeout(()=> {
+    if (screen.width <= 650) 
+      window.location.href = "/home"
+    else 
+      window.location.href = "/desktop"
+  }, 1000)
+
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      Redirecting you to a version that fits you..
+    </div>
+  )
+}
