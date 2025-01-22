84122c188db0f14844e3bcb67612b3007b51a674
Emmanuel Fabella <efrohibire@gmail.com>
2025-01-23 03:56:28 +0800

    feat(api): history fetch, pagination & PUT request data in home

--- client/src/components/layout/home/chat-session.jsx
+++ client/src/components/layout/home/chat-session.jsx
@@ -4,17 +4,28 @@ import InputArea from "../textarea";
 import Notification from "@/components/layout/toast";
 import UserChatBubble from "@/components/features/aiChatComponents/userChatBubble";
 import AiChatBubble from "@/components/features/aiChatComponents/aiChatBubble";
-import { useState, useEffect, useRef } from "react";
-import runChat from "@/config/gemini";
-import { useUserContext } from "@/contexts/useUserContext";
+
 import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import runChat from "@/config/gemini";
 import api from "@/config/axios";
+import { useUserContext } from "@/contexts/useUserContext";
+import { dotStream } from "ldrs";
+dotStream.register();
 
 export default function ChatSession() {
-  const { userInput, aiResponse, setAiResponse, chatHistory, setChatHistory } =
-    useUserContext();
+  const {
+    userInput,
+    setUserInput,
+    setAiResponse,
+    chatHistory,
+    setChatHistory,
+  } = useUserContext();
+
   const [chatData, setChatData] = useState({});
   const { id } = useParams();
+  const [loadingMessageId, setLoadingMessageId] = useState(null);
+
 
   const fetchData = async () => {
     try {
@@ -26,51 +37,64 @@ export default function ChatSession() {
   };
 
   const fetchAIResponse = async () => {
-    if (userInput) {
-      try {
-        // Fetch AI response based on user input
-        const response = await runChat(userInput);
-        setAiResponse(response);
+    if (!userInput) return;
+    const messageId = Date.now().toString();
+    
+    try {
+      const response = await runChat(userInput);
+      setAiResponse(response);
 
-        // Prepare new chat history data
-        const newChatData = [
-          ...(chatData.messages || []),
-          {
-            user: userInput,
-            ai: response
-          }
-        ];
+      // Prepare new chat history data
+      const newChatData = [
+        ...(chatData.messages || []),
+        {
+          id: messageId,
+          user: userInput,
+          ai: response,
+        },
+      ];
 
-        await api
-          .put(`/history/${id}`, {
-            ...chatData,
-            messages: newChatData
-          })
-          .catch((err) => {
-            console.error("Failed to post chat history:", err);
-          });
-      } catch (error) {
-        console.error("Error fetching AI response:", error);
-      }
+      await api
+        .put(`/history/${id}`, {
+          ...chatData,
+          messages: newChatData,
+        })
+        .catch((err) => {
+          console.error("Failed to post chat history:", err);
+        });
+    } catch (error) {
+      console.error("Error fetching AI response:", error);
+    } finally {
+      setLoadingMessageId(null);
     }
   };
 
+  // POST THE DATA 
+  const postData = async () => {
+    if (!userInput) return;
+
+  }
+
   useEffect(() => {
     fetchData();
-    fetchAIResponse();
+    // if (!id)
+    fetchAIResponse(); // Put data
   }, [userInput]);
 
   return (
     <Card className="relative h-[calc(100%-160px)] bg-white rounded-t-2xl rounded-b-3xl mx-3 mt-2 border border-neutral-200">
       <CardHeader className="flex flex-row items-center justify-between px-4 py-5 leading-tight">
         <p className="leading-3 text-sm text-[#030303] font-medium">
-          CATEGORY
+          {chatData.category}
           <br />
           <span className="text-xl font-semibold tracking-tight leading-tight">
             {chatData.title}
           </span>
         </p>
-        <Link to="/home">
+        <Link 
+          reloadDocument
+          to="/home"
+        >
           <Button className="text-md rounded-3xl px-6 bg-[#F4BE37] text-white hover:bg-[#F4BE37]/90">
             + New
           </Button>
@@ -80,13 +104,22 @@ export default function ChatSession() {
       <main className="relative h-[calc(100%-90px)] p-4 flex flex-col justify-end">
         <Notification success={true} />
         <div className="h-full overflow-y-auto">
-          {chatData.messages &&
-            chatData.messages.map((chat, index) => (
-              <div key={index}>
-                <UserChatBubble message={chat.user} />
-                <AiChatBubble message={chat.ai} />
-              </div>
-            ))}
+          {chatData.messages && chatData.messages.map((chat, index) => (
+            <div key={index}>
+              <UserChatBubble message={chat.user} />
+              {chat.id === loadingMessageId ? (
+                <div className="flex justify-start p-4">
+                  <l-dot-stream
+                    size="60"
+                    speed="2.5"
+                    color="black"
+                  ></l-dot-stream>
+                </div>
+              ) : (
+                chat.ai && <AiChatBubble message={chat.ai} />
+              )}
+            </div>
+          ))}
           <div></div>
         </div>
         <InputArea />
--- client/src/components/layout/nav.jsx
+++ client/src/components/layout/nav.jsx
@@ -6,17 +6,17 @@ import { Link } from 'react-router-dom'
 
 export default function nav({page}) {
   return (
-    <footer className="absolute left-0 bottom-0 w-full flex items-center justify-evenly h-20 bg-white border-t border-stone-200">
+    <footer className="absolute left-0 bottom-0 w-full flex items-center justify-evenly h-16 bg-white border-t border-stone-200">
       <Link 
         to="/home"
-        className={`relative flex flex-col items-center justify-center p-4 px-14 ${ page == "home" ? `bg-stone-200 rounded-t-3xl` : ``}`}
+        className={`relative flex flex-col items-center justify-center p-4 px-14 ${ page == "home" ? `bg-slate-200 rounded-t-[2em]` : ``}`}
       >
         <Sparkles className="h-8 w-8" />
         <span className="text-xs tracking-tight font-medium">Generate</span>
       </Link>
       <Link 
         to="/view"
-        className={`relative flex flex-col items-center justify-center p-4 px-14 ${ page == "view" ? `bg-stone-200 rounded-t-3xl` : ``}`}
+        className={`relative flex flex-col items-center justify-center p-4 px-14 ${ page == "view" ? `bg-slate-200 rounded-t-[2em]` : ``}`}
       >
         <LineChart className="h-8 w-8" />
         <span className="text-xs tracking-tight font-medium">Views</span>
