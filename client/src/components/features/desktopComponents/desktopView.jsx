342e043022f124adc84cb490f0d05cfa0f7a74cb
Emmanuel Fabella <efrohibire@gmail.com>
2025-01-30 06:00:14 +0800

    fix: dashboard carousel

--- client/src/components/features/desktopComponents/desktopView.jsx
+++ client/src/components/features/desktopComponents/desktopView.jsx
@@ -3,13 +3,19 @@ import Header from '@/components/features/desktopComponents/header';
 import { Card, CardTitle } from '@/components/ui/card';
 import DashboardContent from './dashboardItem';
 import Carousel1 from './carousel';
+import { useIntroStore } from '@/store/useIntro';
+
 export default function Desktop() {
+  const carousel = useIntroStore((state) => state.carousel)
+
   return (
     <main className="h-screen w-full relative">
       <Header name="User"  />
       <div className='h-full flex items-center '>
         <DashboardContent/>
-        <Carousel1/>
+        {carousel &&
+          <Carousel1/>
+        }
       </div>
     </main>
   );
